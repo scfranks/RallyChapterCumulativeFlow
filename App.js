015a0cecc95c007049d4acea472cd9d6de1bcaa7
scfranks <steven.c.franks@intel.com>
2014-09-15 20:10:25 +0000

    first commit that displays text

---
+++ App.js
@@ -0,0 +1,9 @@
+Ext.define('CustomApp', {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+    items:{ html:"My first RallyChapterCumulativeFlow app2"},
+    launch: function() {
+        //Write app code here
+        console.log("Our first app has arrived3!!");
+    }
+});
