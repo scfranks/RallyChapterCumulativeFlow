19734d0f9ed26dfe414a0e16681908674ff4e44a
scfranks <steven.c.franks@intel.com>
2014-09-15 20:25:04 +0000

    added some drop downs

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
-    items:{ html:"My first RallyChapterCumulativeFlow app2"},
+    items:[{ html:"My first RallyChapterCumulativeFlow app2"},
+        {xtype: 'rallyportfolioitemtypecombobox'},
+        {xtype: 'rallyiterationcombobox'}
+    ],
     launch: function() {
         //Write app code here
         console.log("Our first app has arrived3!!");
